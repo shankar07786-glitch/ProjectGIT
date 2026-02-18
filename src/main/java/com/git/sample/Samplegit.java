4d7727a0b127e46d9847a35e8926d999ba11fdb4
shankar07786-glitch <shankar07786@gmail.com>
2026-02-18 13:55:39 +0530

    New Project

---
+++ src/main/java/com/git/sample/Samplegit.java
@@ -0,0 +1,16 @@
+package com.git.sample;
+
+public class Samplegit {
+	private void project1() {
+		System.out.println("one");
+
+	}
+	private void project2() {
+		System.out.println("two");
+
+	}
+	private void project3() {
+		System.out.println("three");
+
+	}
+}
